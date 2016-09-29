af95acda8c1377c6d853a72de91b855c3d765ac3
JaylanT <Jaylan.Tse@gmail.com>
2016-09-28 18:30:07 -0700

    SRT algorithm implemented. Please check my work.

--- SRT/Tester.java
+++ SRT/Tester.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Random;
 
 /**
@@ -7,30 +6,49 @@ import java.util.Random;
  */
 public class Tester {
 
-    private static final int JOB_COUNT = 10;
+    private static final int JOB_COUNT = 20;
     private static final int MAX_ARRIVAL_TIME = 99;
 
     public static void main(String[] args) {
-        Process[] processes = createProcesses();
+        Process[] processes = new Process[JOB_COUNT];
+        createProcesses(processes);
+        ShortestRemainingTime srt = new ShortestRemainingTime();
 
         for (Process p : processes) {
             System.out.println(p.toString());
         }
+
+        int scheduledCount = 0;
+        for (int i = 0; i <= MAX_ARRIVAL_TIME || srt.hasRemainingJobs(); i++) {
+            if (i <= MAX_ARRIVAL_TIME) {
+                // Multiple jobs may have same arrival time.
+                // Make sure to schedule them all.
+                while (scheduledCount != JOB_COUNT) {
+                    Process nextProcess = processes[scheduledCount];
+                    if (nextProcess.getArrivalTime() == i) {
+                        srt.schedule(nextProcess);
+                        scheduledCount++;
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+            srt.incrementRunTime();
+        }
     }
 
-    private static Process[] createProcesses() {
-        Process[] p = new Process[JOB_COUNT];
+    private static void createProcesses(Process[] p) {
         Random rand = new Random();
         for (int i = 0; i < p.length; i++) {
             int arrivalTime = rand.nextInt(MAX_ARRIVAL_TIME + 1);
             double serviceTime = (rand.nextInt(100) + 1) / 10.0;
             int priority = rand.nextInt(4) + 1;
+            char id = (char)(i + 'A');
 
-            p[i] = new Process(arrivalTime, serviceTime, priority);
+            p[i] = new Process(arrivalTime, serviceTime, priority, id);
         }
 
         Arrays.sort(p, Process::compareTo);
-
-        return p;
     }
 }
